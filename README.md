0651f1041969e13b37e261f237ad5367f529366e
Triple-Z <me@triplez.cn>
2020-10-21 20:00:09 +0800

    AC No.3 with a better solution

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 LeetCode 练习仓库。
 
-> Last updated: 2020-10-21 06:16:42.395986 UTC
+> Last updated: 2020-10-21 12:00:09.622667 UTC
 >
 > Run `make update` to update this file, or just make a Git commit (if the `pre-commit` hooks are installed).
 
--- java/src/3. LongestSubstringWithoutRepeatingCharacters.java
+++ java/src/3. LongestSubstringWithoutRepeatingCharacters.java
@@ -1,30 +1,24 @@
 class Solution {
     // pointers: [left, right)
     private int left = 0;
-    private int right = 1;
-    private int max = 1;
+    private int right = 0;
+    private int max = Integer.MIN_VALUE;
     private Map<Character, Integer> counters = new HashMap<>();
 
     public int lengthOfLongestSubstring(String s) {
         if (s.length() < 1) return 0;
-        counters.put(s.charAt(0), 1);
 
-        while (right < s.length() && left < right) {
+        while (right < s.length() && left <= right) {
             char cRight = s.charAt(right);
             char cLeft = s.charAt(left);
             if (!counters.containsKey(cRight)) {
                 // move right
                 counters.put(cRight, 1);
                 right++;
-            } else if (right - left > 1) {
+            } else {
                 // move left
                 counters.remove(cLeft);
                 left++;
-            } else {
-                // move left & right
-                counters.remove(cLeft);
-                counters.put(cRight, 1);
-                left++; right++;
             }
 
             // calc
