62154127ff72955fa47565054a38d36ea1938866
Triple-Z <me@triplez.cn>
2021-06-03 13:38:55 +0800

    AC No.104 with Go
    Signed-off-by: Triple-Z <me@triplez.cn>

---
+++ go/src/104.go
@@ -0,0 +1,14 @@
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
