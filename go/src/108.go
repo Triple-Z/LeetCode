f4a7217577309f9e2fd6d685460e5fbf31be53df
Triple-Z <me@triplez.cn>
2021-06-03 14:29:10 +0800

    AC No.108 with Go
    Signed-off-by: Triple-Z <me@triplez.cn>

---
+++ go/src/108.go
@@ -0,0 +1,12 @@
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	mid := len(nums) / 2
+	return &TreeNode{
+		Val:   nums[mid],
+		Left:  sortedArrayToBST(nums[:mid]),
+		Right: sortedArrayToBST(nums[mid+1:]),
+	}
+}
