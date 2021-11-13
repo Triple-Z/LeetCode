b68e2927f362a0d82f89641f2f81557610916980
Triple-Z <me@triplez.cn>
2021-11-13 19:25:06 +0800

    AC No.343 & LCOF No.14-I with Go
    Signed-off-by: Triple-Z <me@triplez.cn>

---
+++ go/src/343.go
@@ -0,0 +1,17 @@
+func integerBreak(n int) int {
+	if n == 2 {
+		return 1
+	} else if n == 3 {
+		return 2
+	}
+
+	rope3 := n / 3
+
+	if n%3 == 1 { // at last, 4 is a better choice
+		rope3--
+	}
+
+	rope2 := (n - 3*rope3) / 2
+
+	return int(math.Pow(3.0, float64(rope3))) * int(math.Pow(2.0, float64(rope2)))
+}
